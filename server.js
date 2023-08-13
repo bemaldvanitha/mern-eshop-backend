2c79c24fb0e94e5e3936770b1782f22e0c96ffe9
bemaldvanitha <bdvidanagamachchi@gmail.com>
2023-08-13 18:42:22 +0530

    basic mock data send api setup

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 import express from 'express';
 
+import products from "./data/products.js";
+
 const PORT = 5000;
 
 const app = express();
 
-app.get('', (req, res) => {
+app.get('/', (req, res) => {
    res.send("API running");
+});
+
+app.get('/api/products', (req, res) => {
+   res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+   const id = req.params.id;
+   const product = products.find(product => product._id === id);
+   res.json(product);
 });
 
 app.listen(PORT, () => {
