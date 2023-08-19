ab70daadc4081c3c027635cb5748bffd76a864e1
bemaldvanitha <bdvidanagamachchi@gmail.com>
2023-08-19 17:45:04 +0530

    connect to mongodb and fetch data from that

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
@@ -20,15 +20,7 @@ app.get('/', (req, res) => {
    res.send("API running");
 });
 
-app.get('/api/products', (req, res) => {
-   res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-   const id = req.params.id;
-   const product = products.find(product => product._id === id);
-   res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 app.listen(PORT, () => {
    console.log(`Server running on ${PORT}`);
