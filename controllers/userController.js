4f86003447a1c61a5ccdeb9403f0c555b986e5d4
bemal-engenuityai <141420637+bemal-engenuityai@users.noreply.github.com>
2024-04-09 06:11:19 +0530

    jwt and cookie added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken';
 
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
@@ -17,6 +18,20 @@ const authUser = asyncHandler(async (req, res) => {
         const isPasswordMatched = await bcrypt.compare(password, user.password);
 
         if(isPasswordMatched){
+            const token = jwt.sign({
+                userId: user._id
+            }, process.env.JWT_SECRET, {
+                expiresIn: '30d'
+            });
+
+            // set jwt as http only cookie
+            res.cookie('jwt', token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV !== 'development',
+                sameSite: 'strict',
+                maxAge: 30 * 24 * 60 * 60 * 1000
+            });
+
             return res.json({
                 _id: user._id,
                 name: user.name,
