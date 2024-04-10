026ecea5381bf5c2bf55a819bc7404a8580fae62
bemal-engenuityai <141420637+bemal-engenuityai@users.noreply.github.com>
2024-04-10 05:33:35 +0530

    middleware added to autherize and admin

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,17 +3,18 @@ import express from "express";
 import { getUserById, getUserProfile, getUsers, authUser,
     registerUser, updateUserProfile, updateUser, logoutUser,
     deleteUser } from '../controllers/userController.js';
+import {protect, admin} from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.get('/', getUsers);
+router.get('/', protect, admin, getUsers);
 router.post('/', registerUser);
 router.post('/logout', logoutUser);
 router.post('/login', authUser);
-router.get('/profile', getUserProfile);
-router.put('/profile', updateUserProfile);
-router.get('/:id', getUserById);
-router.put('/:id', updateUser);
-router.delete('/:id', deleteUser);
+router.get('/profile', protect ,getUserProfile);
+router.put('/profile', protect ,updateUserProfile);
+router.get('/:id',protect, admin, getUserById);
+router.put('/:id',protect, admin, updateUser);
+router.delete('/:id',protect, admin, deleteUser);
 
 export default router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
@@ -20,6 +21,8 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
    res.send("API running");
