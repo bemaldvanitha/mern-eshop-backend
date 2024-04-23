25e40e3cf1b873a00935f86d20702fba6e5ea9ff
bemal-engenuityai <141420637+bemal-engenuityai@users.noreply.github.com>
2024-04-24 04:59:54 +0530

    create new product added

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,14 @@
 import express from "express";
 
-import { getProducts, getProductById } from '../controllers/productController.js';
+import { getProducts, getProductById, createProduct } from '../controllers/productController.js';
+import {protect, admin} from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.get('/', getProducts);
 
 router.get('/:id', getProductById);
+
+router.post('/', protect, admin, createProduct);
 
 export default router;
