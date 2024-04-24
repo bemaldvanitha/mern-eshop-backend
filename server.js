e77ebc1413a1c8c9056029512d1596400556753a
bemal-engenuityai <141420637+bemal-engenuityai@users.noreply.github.com>
2024-04-24 05:39:24 +0530

    upload image riyre

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import path from "path";
 
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 dotenv.config();
 
@@ -30,6 +32,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
 app.get('/', (req, res) => {
    res.send("API running");
 });
@@ -37,6 +42,7 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 app.get('/api/config/paypal', (req, res) => res.send({
    clientId: process.env.PAYPAL_CLIENT_ID
 }));
